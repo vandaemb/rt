ddcfc59b4d40a2fac1675a529ac7820644b0f2de
Ruslan Zakirov <ruz@bestpractical.com>
2011-12-28 00:05:07 +0400

    scrips disabling in the UI and API

--- lib/RT/Scrip.pm
+++ lib/RT/Scrip.pm
@@ -348,6 +348,33 @@ sub Stage {
     return undef;
 }
 
+=head2 Disabled
+
+Returns the current value of Disabled. 
+
+=cut
+
+sub Disabled {
+    my $self = shift;
+    my $record = RT::ObjectScrip->new( $self->CurrentUser );
+    $record->LoadByCols( Scrip => $self->id );
+    return $record->Disabled;
+}
+
+=head2 SetDisabled
+
+Takes a boolean.
+1 will cause this scrip to no longer be avaialble for objects.
+0 will re-enable this field.
+
+=cut
+
+sub SetDisabled {
+    my $self = shift;
+    return RT::ObjectScrip->new( $self->CurrentUser )
+        ->SetDisabledOnAll( Scrip => $self->id, Value => shift );
+}
+
 
 =head2 Apply { TicketObj => undef, TransactionObj => undef}
 
