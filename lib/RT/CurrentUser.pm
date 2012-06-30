842a114c128f35f0b65ee7d9f7a91d3ea27e23b2
sunnavy <sunnavy@bestpractical.com>
2012-06-30 18:50:22 +0800

    return the unescaped string if it's not in .po
    this fixes the bug that returns "~[Up~]" instead of the right "[Up]"

--- lib/RT/CurrentUser.pm
+++ lib/RT/CurrentUser.pm
@@ -242,7 +242,9 @@ sub loc {
     if (@_ == 1) {
         # pre-scan the lexicon hashes to return _AUTO keys verbatim,
         # to keep locstrings containing '[' and '~' from tripping over Maketext
-        return $_[0] unless grep exists $_->{$_[0]}, @{ $handle->_lex_refs };
+        my $unescaped = $_[0];
+        $unescaped =~ s!~(.)!$1!g;
+        return $unescaped unless grep exists $_->{$_[0]}, @{ $handle->_lex_refs };
     }
 
     return $handle->maketext(@_);
